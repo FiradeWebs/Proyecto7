82d39674edb341a56cf0b34f62f109396f71ea2b
Miguel Tello Sanz <10000714.joan23@fje.edu>
2017-03-21 17:34:32 +0100

    Registro en PHP

---
+++ registro/php/proc/register.proc.php
@@ -0,0 +1,25 @@
+<?php 
+extract($_POST);
+	require_once('conexion.php');
+	//primero comprobamos que el usuario no exista
+		$con="SELECT * FROM `tbl_usuario` WHERE `correo_usuario` LIKE '".$s_email."'";
+		//echo $con;
+	//Lanzamos la consulta a la BD
+		$result	=	mysqli_query($conexion,$con);
+	//Contamos los resultados que nos devuelve
+		$total  = mysqli_num_rows($result);
+	//Comprobamos si el resultado ya existe
+	if($total>=1){
+		echo "<script type='text/javascript'>alert('Ya se encuentra registrado');
+			location.href='../../index.php';</script>";
+	}
+		//Si no existe, lo añadimos a la base de datos
+	else{
+		//preparamos la consulta de inserción
+		$inser_con="INSERT INTO `tbl_usuario` (`usu_nombre`, `usu_apellidos`, `usu_sexo`, `usu_correo`, `usu_direccion`, `usu_nickname`, `usu_pass`) VALUES ('".$s_name."', '".$s_lastname1."', '".$s_lastname2."', '".$s_email."', '".$s_dirección."', '".$s_nickname."', '".$s_password."');";
+		//echo $inser_con;die;
+		mysqli_query($conexion,$inser_con);
+		echo "<script type='text/javascript'>alert('registro completado');
+			location.href='../../index.php';</script>";
+	}
+ ?>
