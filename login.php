2b025a14ed5514803e82e24b2569dc8f4cfd8e68
ARodriguez97 <4800.joan23@fje.edu>
2017-03-22 17:42:32 +0100

    LOGIN RESPONSIVE

--- login.php
+++ login.php
@@ -41,49 +41,66 @@
 			<link rel="stylesheet" href="./css/costum.css">
 	</head>
 	<body>
-		<div class="container">
-		  <div class="row">
+		<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 
-		    <div class="main">
-					<img src="./img/logo_17-03(v2).png">
-		      <h3>Please Log In, or <a href="#">Sign Up</a></h3>
-		      <div class="row">
-		        <div class="col-xs-6 col-sm-6 col-md-6">
-		          <a href="#" class="btn btn-lg btn-primary btn-block">Facebook</a>
-		        </div>
-		        <div class="col-xs-6 col-sm-6 col-md-6">
-		          <a href="#" class="btn btn-lg btn-info btn-block">Google</a>
-		        </div>
-		      </div>
-		      <div class="login-or">
-		        <hr class="hr-or">
-		        <span class="span-or">or</span>
-		      </div>
+<div class="container">
 
-		      <form name="login" action="php/iniciar_sesion_proc.php" method="POST" role="form">
-		        <div class="form-group">
-		          <label for="inputUsernameEmail">Username or email</label>
-		          <input type="text" class="form-control name" name="name" id="name" maxlength="15">
-		        </div>
-		        <div class="form-group">
-		          <a class="pull-right" href="#">Forgot password?</a>
-		          <label for="inputPassword">Password</label>
-		          <input type="password" class="form-control pass" name="pass" id="pass" >
-		        </div>
-		        <div class="checkbox pull-right">
-		          <label>
-		            <input type="checkbox">
-		            Remember me </label>
-		        </div>
-		        <button type="submit" class="btn btn btn-primary send" id="send" value="Acceder">
-		          Log In
-		        </button>
-		      </form>
 
-		    </div>
-
-		  </div>
+    <div class="omb_login">
+			<img src="./img/logo_17-03(v2).png">
+    	<h3 class="omb_authTitle">Login or <a href="#">Sign up</a></h3>
+		<div class="row omb_row-sm-offset-3 omb_socialButtons">
+    	    <div class="col-xs-4 col-sm-2">
+		        <a href="#" class="btn btn-lg btn-block omb_btn-facebook">
+			        <i class="fa fa-facebook visible-xs"></i>
+			        <span class="hidden-xs">Facebook</span>
+		        </a>
+	        </div>
+        	<div class="col-xs-4 col-sm-2">
+		        <a href="#" class="btn btn-lg btn-block omb_btn-twitter">
+			        <i class="fa fa-twitter visible-xs"></i>
+			        <span class="hidden-xs">Twitter</span>
+		        </a>
+	        </div>
+        	<div class="col-xs-4 col-sm-2">
+		        <a href="#" class="btn btn-lg btn-block omb_btn-google">
+			        <i class="fa fa-google-plus visible-xs"></i>
+			        <span class="hidden-xs">Google+</span>
+		        </a>
+	        </div>
 		</div>
+
+		<div class="row omb_row-sm-offset-3 omb_loginOr">
+			<div class="col-xs-12 col-sm-6">
+				<hr class="omb_hrOr">
+				<span class="omb_spanOr">or</span>
+			</div>
+		</div>
+
+		<div class="row omb_row-sm-offset-3">
+			<div class="col-xs-12 col-sm-6">
+			    <form class="omb_loginForm" action="" autocomplete="off" method="POST">
+					<div class="input-group">
+						<span class="input-group-addon"><i class="fa fa-user"></i></span>
+						<input type="text" class="form-control" name="username" placeholder="email address">
+					</div>
+					<span class="help-block"></span>
+
+					<div class="input-group">
+						<span class="input-group-addon"><i class="fa fa-lock"></i></span>
+						<input  type="password" class="form-control" name="password" placeholder="Password">
+					</div>
+          <br>
+
+					<button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+				</form>
+			</div>
+    	</div>
+	</div>
+
+
+
+        </div>
 
 
 	</body>
