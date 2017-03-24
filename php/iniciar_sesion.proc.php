3d2669d6cdd6ab0d74f4350ab74300c69243942e
ARodriguez97 <4800.joan23@fje.edu>
2017-03-24 18:12:57 +0100

    Este es el BUENO (javi) :+1:
    zizi

--- php/iniciar_sesion.proc.php
+++ php/iniciar_sesion.proc.php
@@ -14,8 +14,7 @@
 				$_SESSION['usu_id']	=	$fila[0];
 				//Redireccionamos
 
-				header("location:");
-				print_r("Hola");
+				header("location:discover.php");
 			}
 		}
 		//Si no nos devuelve registros significa que el usuario o contraseña han sido incorrectos.
