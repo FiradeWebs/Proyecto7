64d306accbe57eb6801738e2294487cc1d7c8bbc
Aaron4786 <4786.joan23@fje.edu>
2017-03-24 19:32:01 +0100

    JavaScript y Ajax

--- js/main.js
+++ js/main.js
@@ -1,16 +1,42 @@
 /**
  * jTinder initialization
  */
+ var id = $('#id').val();
+
 $("#tinderslide").jTinder({
 	// dislike callback
     onDislike: function (item) {
+    	var dec = "No";
+
+    	$.ajax({
+                    type: "POST",
+                    url: "insert_interes.proc.php?dec=" + dec,
+                    data: $("#cat_id").serialize(),// Adjuntar los campos del formulario enviado.
+                    success: function(data){
+                    	//var id = $('#id').val();
+                    	 $('#status').html('Dislike image ' + (item.index()+1));
+
+                                
+            }
+        });
 	    // set the status text
-        $('#status').html('Dislike image ' + (item.index()+1));
-    },
+        },
 	// like callback
     onLike: function (item) {
+    	var dec = "Si";
+    	$.ajax({
+                    type: "POST",
+                    url: "insert_interes.proc.php?dec=" + dec,
+                     // Adjuntar los campos del formulario enviado.
+                    success: function(data){
+                    	 $('#status').html('Like image ' + (item.index()+1));
+                    	 
+
+                                
+            }
+        });
 	    // set the status text
-        $('#status').html('Like image ' + (item.index()+1));
+	   
     },
 	animationRevertSpeed: 200,
 	animationSpeed: 400,
