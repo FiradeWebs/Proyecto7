a378f4df32f5167b11a4d94be539414e1eaa0435
ARodriguez97 <4800.joan23@fje.edu>
2017-03-27 16:21:28 +0200

    Cambios del CSS

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 		<script>
@@ -50,36 +49,15 @@
     <div class="omb_login">
 			<img src="./img/logo_17-03(v2).png">
     	<h5 class="omb_authTitle">Inicia Sesión o<a href="./php/registro.php"> Registrate</a></h5>
-		<div class="row omb_row-sm-offset-3 omb_socialButtons">
-    	    <div class="col-xs-4 col-sm-2">
-		        <a href="#" class="btn btn-lg btn-block omb_btn-facebook">
-			        <i class="fa fa-facebook visible-xs"></i>
-			        <span class="hidden-xs">Facebook</span>
-		        </a>
-	        </div>
-        	<div class="col-xs-4 col-sm-2">
-		        <a href="#" class="btn btn-lg btn-block omb_btn-twitter">
-			        <i class="fa fa-twitter visible-xs"></i>
-			        <span class="hidden-xs">Twitter</span>
-		        </a>
-	        </div>
-        	<div class="col-xs-4 col-sm-2">
-		        <a href="#" class="btn btn-lg btn-block omb_btn-google">
-			        <i class="fa fa-google-plus visible-xs"></i>
-			        <span class="hidden-xs">Google+</span>
-		        </a>
-	        </div>
-		</div>
-
 		<div class="row omb_row-sm-offset-3 omb_loginOr">
-			<div class="col-xs-12 col-sm-6">
+			<div class="">
 				<hr class="omb_hrOr">
-				<span class="omb_spanOr">o</span>
+				<span class="omb_spanOr"></span>
 			</div>
 		</div>
 
 		<div class="row omb_row-sm-offset-3">
-			<div class="col-xs-12 col-sm-6">
+			<div class="">
 			    <form class="omb_loginForm" name="f1" action="php/iniciar_sesion.proc.php" method="POST">
 					<div class="input-group">
 						<span class="input-group-addon"><i class="fa fa-user"></i></span>
